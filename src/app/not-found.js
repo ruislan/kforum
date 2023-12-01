23fd41750e4623cecdc8ee701e245ceda80bc2dc
Ruislan <z17520@126.com>
2023-12-01 16:16:14 +0800

    fix not-found page bugs

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,17 +1,30 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import { siteSettingModel } from '@/lib/models';
 
-export default function NotFound() {
+async function getLogo() {
+    return await siteSettingModel.getFieldValue(siteSettingModel.fields.siteLogo);
+}
+
+export default async function NotFound() {
+    const logo = await getLogo();
     return (
         <div className='m-32 flex flex-col w-full max-w-2xl'>
             <Link href='/' className='relative font-bold text-lg text-white mr-4 flex items-center group max-w-[156px]'>
-                <div className='absolute w-12 h-12 rounded-md bg-neutral-600 shadow-lg left-0 group-hover:w-full transition-[width]'></div>
-                <span className='z-10 w-12 h-12 text-3xl flex justify-center items-center mr-1 group-hover:-rotate-12 transition-all duration-500'>K</span>
-                <span className='z-10 text-3xl pr-3'>Forum</span>
+                {logo ?
+                    <Image className='h-9 w-auto max-h-9' src={logo} alt={logo} width={180} height={60} /> :
+                    <>
+                        <div className='absolute w-12 h-12 rounded-md bg-neutral-600 shadow-lg left-0 group-hover:w-full transition-[width]'></div>
+                        <span className='z-10 w-12 h-12 text-3xl flex justify-center items-center mr-1 group-hover:-rotate-12 transition-all duration-500'>K</span>
+                        <span className='z-10 text-3xl pr-3'>Forum</span>
+                    </>
+                }
             </Link>
             <h2 className='text-2xl font-bold mt-8 mb-4'>无法访问</h2>
             <p className='text-lg'>您访问了一个未知页面，或者该页面已经删除</p>
-            <div className='flex items-center mt-4'>
+            <div className='flex items-center mt-4 gap-2'>
                 <Link href='/' className='underline underline-offset-2 font-semibold'>去首页</Link>
+                <Link href='/search' className='underline underline-offset-2 font-semibold'>去搜索</Link>
             </div>
         </div>
     );
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,11 @@ export default async function Footer() {
     return (
         <div className='w-screen flex justify-center py-12'>
             <div className='flex flex-col-reverse md:flex-row gap-3 items-center justify-center md:gap-8 text-sm'>
-                <span className='text-gray-400'>@2023 KForum. Built with Love By <Link href='https://ruislan.com' className='underline underline-offset-2'>Rui</Link></span>
+                <span className='text-gray-400'>@2023&nbsp;
+                    <Link href='https://github.com/ruislan/kforum' className='underline underline-offset-2'>KForum</Link>
+                    &nbsp;. Built with Love By&nbsp;
+                    <Link href='https://ruislan.com' className='underline underline-offset-2'>Rui</Link>
+                </span>
                 <div className='flex items-center gap-2'>
                     <Link className='text-gray-200 underline underline-offset-2' href='https://github.com/ruislan/kforum'>源码</Link>
                     <Link className='text-gray-200 underline underline-offset-2' href='#'>社区规则</Link>
