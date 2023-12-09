3387eb948d45e6da18d222061ada1adc22acd695
Ruislan <z17520@126.com>
2023-12-09 23:54:36 +0800

    rename LocalStorage to DiskStorage

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import logger from './logger';
 
 // XXX maybe we can try a cloud storage like qiniu, superbase, s3, etc.
-class LocalStorage {
+class DiskStorage {
     constructor({ storeBase, relativePathBase }) {
         this.storeBase = storeBase || './public/uploads';
         this.relativePathBase = relativePathBase || '/uploads';
@@ -20,21 +20,21 @@ class LocalStorage {
         if (!fs.existsSync(dir)) await fs.promises.mkdir(dir, { recursive: true });
         await fs.promises.writeFile(`${dir}/${filename}`, buffer);
         const filePath = `${this.relativePathBase}/${year}/${month}/${day}/${filename}`;
-        logger.info(`storage:local File ${filename} is store to dir ${dir}, relative path is ${filePath} .`);
+        logger.info(`storage:disk File ${filename} is store to dir ${dir}, relative path is ${filePath} .`);
         return filePath;
     }
     async delete(filename) {
         const path = filename.substring(this.relativePathBase.length);
         const dir = `${this.storeBase}${path}`;
         await fs.promises.unlink(dir);
-        logger.info(`storage:local File ${filename} is deleted`);
+        logger.info(`storage:disk File ${filename} is deleted`);
     }
 }
 
 const isProdEnv = process.env.NODE_ENV === 'production';
 
 const globalForStorage = global;
-const storage = globalForStorage.storage ?? new LocalStorage({});
+const storage = globalForStorage.storage ?? new DiskStorage({});
 if (!isProdEnv) globalForStorage.storage = storage;
 
 export default storage;
